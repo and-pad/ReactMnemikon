36c2c38443879e935308a2bc1fa06ff5d10d8080
and-pad <andrestor@gmail.com>
2026-05-20 22:18:11 +0000

    Docker config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,21 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+
+    hmr: {
+    protocol: 'ws',
+    host: '192.168.1.217',
+    clientPort: 80
+  }
+  },
+
   build: {
-    chunkSizeWarningLimit: 1000, // opcional
+    chunkSizeWarningLimit: 1000,
+
     rollupOptions: {
       output: {
         manualChunks: {
